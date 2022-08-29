23123e7050ceca2ea72727047dc3012fb6e115e9
Sheena Sang <sheenasany@gmail.com>
2022-08-29 17:42:22 -0400

    commit tree 58af1e8d8e2fb222907e9f64c47a2d2b1470fab2

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+dwarf_names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(dwarf_names)
+ dwarf_names.each.with_index do |dwarf, index|
+  puts "#{index+1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |planeteer|
+    "#{planeteer.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+short_words = ["puff", "go", "two"]
+assorted_words = ["two", "go", "industrious", "bop"]
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+ingredients = ["garlic", "rosemary", "bread"]
+soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+snacks = ["crackers", "gouda", "thyme"]
+def find_the_cheese(cheeses)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
+
+binding.pry
